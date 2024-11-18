02d39039ae0bf23b0c27516200178d1a2110b69a
Utkarsh Sharma <utkarsh4033@gmail.com>
2024-11-18 14:27:28 +0530

    updates

--- 02counterapp/src/App.jsx
+++ 02counterapp/src/App.jsx
@@ -2,25 +2,21 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  
-  let [counter, setCounter] = useState(15);
+  const [counter, setCounter] = useState(15);
   const addValue = () => {
     if (counter == 20) {
-      setCounter(counter =20)
+      setCounter((counter = 20));
     } else {
-      setCounter(counter + 1 )
+      setCounter(counter + 1);
     }
   };
-  const removeValue = () =>{
-      
-      if(counter == 0){
-        setCounter(counter= 0)
-      }
-      else{
-        setCounter(counter -1)
-      }
-      
-  }
+  const removeValue = () => {
+    if (counter == 0) {
+      setCounter((counter = 0));
+    } else {
+      setCounter(counter - 1);
+    }
+  };
   return (
     <>
       <h1>chai aur React</h1>
